e7c00b85ccc0fc5eefb80f3a0f2037441c0caa54
vickydev03 <vickysinghdev03@gmail.com>
2025-06-16 02:36:49 +0530

    added navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import DashboardNavbar from "@/components/DashboardNavbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import DashboardSidebar from "@/modules/dashboard/ui/component/DashboardSidebar";
 import React from "react";
@@ -5,8 +6,9 @@ import React from "react";
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <main className="flex  h-screen w-screen bg-muted">
-        <DashboardSidebar/>
+      <DashboardSidebar />
+      <main className="  h-screen w-screen bg-muted">
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
